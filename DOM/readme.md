4b5cf43427649cfd01724376c69d93af969f39d5
rick <rickbhowmik4.com>
2025-05-09 15:46:03 +0530

    3 pillars of dom

---
+++ DOM/readme.md
@@ -0,0 +1,7 @@
+there are four feelings of dom (document object model )
+
+selection 
+changing inner html
+changing css 
+event listner (need to capture any harkat in screen like click, hover, scroll etc.)
+
---
+++ DOM/script.js
@@ -0,0 +1,19 @@
+
+// selecting the h1 element
+
+const h = document.querySelector("h1")
+
+
+// changing the text content of the h1 element
+
+h.innerHTML = "its me i guess!"
+
+
+// changing the color of the h1 element
+
+
+h.style.color =  "pink"
+
+h.style.backgroundColor = "black"
+
+
